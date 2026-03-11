61aef949b1928f1c998ba73023a46377bbfa53fb
SecretiveRabbit <70057193+SecretiveRabbit@users.noreply.github.com>
2026-03-11 19:02:21 +0200

    Include environment slot name in response message

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Builder;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello from .NET 8 Web App!");
+var slot = Environment.GetEnvironmentVariable("WEBSITE_SLOT_NAME") ?? "Unknown";
+
+app.MapGet("/", () => $"Hello from .NET 8 Web App! Slot: {slot}");
 
 app.Run();
